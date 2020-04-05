cc590e5ea64c03b16e368a0c24c62e33ad99c1b7
Inth <inth@o2.pl>
2020-04-05 13:15:17 +0200

    Adding project to Git
    Adding project to Git

---
+++ Giełda2/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Giełda2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "UPDATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CDR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "UMT";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(68, 131);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(65, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PILAB";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(68, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(65, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "WASKO";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(68, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(65, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "This may take a while";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(150, 237);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Giełda2/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giełda2
+{
+    public partial class Form1 : Form
+    {
+        // Adding variables with different URLs to different stocks 
+
+        string urlCDR = @"https://www.gpw.pl/spolka?isin=PLOPTTC00011";
+        string urlUMT = @"https://www.sharewise.com/us/instruments/UMT_United";
+        string urlPILAB = @"https://www.gpw.pl/spolka?isin=PLPILAB00012";
+        string urlWASKO = @"https://www.gpw.pl/spolka?isin=PLHOGA000041";
+        //string path = @"d:\MyTest.txt";
+        string webContentCDR;
+        string webContentUMT;
+        string webContentPILAB;
+        string webContentWASKO;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            {
+                webContentCDR = DownloadWebPage(urlCDR);
+                webContentUMT = DownloadWebPage(urlUMT);
+                webContentPILAB = DownloadWebPage(urlPILAB);
+                webContentWASKO = DownloadWebPage(urlWASKO);
+
+                //--> if you need to preview url code you can paste it to a notepad from clipboard
+                //Clipboard.SetText(webContentUMT);
+
+                //Regular Expressions parsing the code to find a desired value
+
+                string pattern = "<span class=\"summary\">\\d*,\\d\\d";
+                string numberInPattern = "\\d*,\\d\\d";
+                string patternUMT = "<span data-original-title=\"Price\" role=\"tooltip\" data-toggle=\"tooltip\">\\D\\d.\\d\\d</span>";
+                string numberInPatternUMT = "\\D\\d.\\d\\d";
+                
+                //PULLING CODE FOR CDR
+
+                string inputCDR = webContentCDR;
+                Match mCDR = Regex.Match(inputCDR, pattern, RegexOptions.IgnoreCase);
+                if (mCDR.Success)
+                {
+                    Match nCDR = Regex.Match(mCDR.Value, numberInPattern, RegexOptions.IgnoreCase);
+                    textBox1.Text = nCDR.Value;
+                }
+                else
+                {
+                    textBox1.Text = "Invalid";
+                }
+
+                //PULLING CODE FOR PILAB
+
+                string inputPILAB = webContentPILAB;
+                Match mPILAB = Regex.Match(inputPILAB, pattern, RegexOptions.IgnoreCase);
+                if (mPILAB.Success)
+                {
+                    Match nPILAB = Regex.Match(mPILAB.Value, numberInPattern, RegexOptions.IgnoreCase);
+                    textBox4.Text = nPILAB.Value;
+                }
+                else
+                {
+                    textBox4.Text = "Invalid";
+                }
+
+                //PULLING CODE FOR WASKO
+
+                string inputWASKO = webContentWASKO;
+                Match mWASKO = Regex.Match(inputWASKO, pattern, RegexOptions.IgnoreCase);
+                if (mWASKO.Success)
+                {
+                    Match nWASKO = Regex.Match(mWASKO.Value, numberInPattern, RegexOptions.IgnoreCase);
+                    textBox5.Text = nWASKO.Value;
+                }
+                else
+                {
+                    textBox5.Text = "Invalid";
+                }
+
+                //PULLING CODE FOR UMT
+
+                string inputUMT = webContentUMT;
+                Match mUMT = Regex.Match(inputUMT, patternUMT, RegexOptions.IgnoreCase);
+                if (mUMT.Success)
+                {
+                    Match nUMT = Regex.Match(mUMT.Value, numberInPatternUMT, RegexOptions.IgnoreCase);
+                    textBox3.Text = nUMT.Value;
+                }
+                else
+                {
+                    textBox3.Text = "Invalid";
+                }
+            }
+        }
+
+        // This method downloads the code from the website
+        public static string DownloadWebPage(string theURL)
+        {
+            //download a web page to a string
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            Stream data = client.OpenRead(theURL);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            return s;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
